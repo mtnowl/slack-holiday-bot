c973d8141d8f14cf2a700390162109680dc8772a
mtnowl <henry@cadenzadevelopment.com>
2024-09-17 15:50:30 +0800

    Add multiple country support

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -8,25 +8,31 @@ const slackToken = process.env.SLACK_BOT_TOKEN;
 const slackChannel = process.env.SLACK_CHANNEL_ID;
 const slack = new WebClient(slackToken);
 
+const numDays = 30;
+const countries = process.env.COUNTRIES?.split(',');
+
 nagerClient.setConfig({
   // set default base url for requests
   baseUrl: 'https://date.nager.at',
 });
 
-export async function checkAndNotifyHolidays() {
-  const today = new Date();
-  const numDays = 30;
-  const nextMonth = new Date(today.getTime() + numDays * 24 * 60 * 60 * 1000);
+async function checkHolidaysForCountry(country: string) {
   const { data: holidays } = await publicHolidayNextPublicHolidays({
-    path: { countryCode: 'US' },
+    path: { countryCode: country },
   });
+  console.debug(Array.isArray(holidays));
   if (!holidays || holidays.length === 0)
-    return { message: 'No upcoming holidays in the next ${numDays} days' };
+    return { message: 'No upcoming holidays' };
+
+  const today = new Date();
+  const nextMonth = new Date(today.getTime() + numDays * 24 * 60 * 60 * 1000);
 
   const upcomingHolidays = holidays.filter((holiday) => {
     const holidayDate = new Date(holiday.date!);
     return holidayDate >= today && holidayDate <= nextMonth;
   });
+  if (upcomingHolidays.length === 0)
+    return { message: 'No upcoming holidays in the next ${numDays} days' };
 
   // Prepare message
   const message = upcomingHolidays
@@ -34,6 +40,19 @@ export async function checkAndNotifyHolidays() {
       return `• ${holiday.date}: ${holiday.name}`;
     })
     .join('\n');
+
+  return message;
+}
+
+export async function checkAndNotifyHolidays() {
+  const message = (
+    await Promise.all(
+      countries!.map(
+        async (country) =>
+          `--${country}--\n${await checkHolidaysForCountry(country)}`
+      )
+    )
+  ).join('\n');
 
   // Send message to Slack
   await slack.chat.postMessage({
